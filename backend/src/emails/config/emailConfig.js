338acf74d82b87598683b47ae90a9c7a5e6d6924
rockleaks <rock94507@gmail.com>
2026-06-06 22:02:29 +0530

    email config

---
+++ backend/src/emails/config/emailConfig.js
@@ -0,0 +1,5 @@
+export const emailConfig = {
+  companyName: "{{COMPANY_NAME}}",
+  supportEmail: "{{SUPPORT_EMAIL}}",
+  address: "{{ADDRESS}}",
+};
---
+++ backend/src/emails/config/emailTransporter.js
@@ -0,0 +1,16 @@
+import nodemailer from 'nodemailer';
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: Number(process.env.EMAIL_PORT),
+    secure: process.env.EMAIL_SECURE === "true",
+    auth: {
+        user: process.env.EMAIL_ID,
+        pass: process.env.EMAIL_PASS,
+    },
+    tls: {
+        rejectUnauthorized: false,
+    },
+});
+
+export { transporter };
